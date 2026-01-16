6b7e2cc2249f855eb1c85814ac7f3c75ac222b10
llFaytherll <shleakhter@gmail.com>
2026-01-16 16:47:11 +0300

    что-то не так

--- frontend/start-js.js
+++ frontend/start-js.js
@@ -1,5 +1,5 @@
 if (localStorage.getItem("accessToken") != undefined) {
-    await getProtectedData(localStorage.getItem("accessToken"))
+    getProtectedData(localStorage.getItem("accessToken"));
 } else if (localStorage.getItem("refresh_token") != undefined) {
-    await refreshToken(localStorage.getItem("refresh_token"))
+    refreshToken(localStorage.getItem("refresh_token"));
 } else {create_sign_in_container()}
