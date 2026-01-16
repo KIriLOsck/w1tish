c2fa0018490e491b639224c6ade6ec15525ec867
llFaytherll <shleakhter@gmail.com>
2026-01-16 16:40:10 +0300

    пояснил на все вопросы бэкендеру

--- frontend/js-backend.js
+++ frontend/js-backend.js
@@ -31,7 +31,6 @@ async function login(username, password) {
         console.log("Виноват бэкэндер")
     } else {
         const data = await response.json();
-    // Сохраняем access token только в памяти приложения
         localStorage.setItem("accessToken", data.access_token);
         localStorage.setItem("refresh_token", data.refresh_token);
 
@@ -45,15 +44,16 @@ async function getProtectedData(token) {
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ token }) //проверяй не undefined ли токен
+        body: JSON.stringify({ token }) 
     });
 
-    if (response.status === 401 || response.status === 422) { //422 это когда токена нету куттулу а не когда его надо обновить исправляй давай
+    if (response.status === 401) { 
         console.log("Нужно обновить токен (refresh)");
-    // Здесь вызывается функция обновления токена через куку
         await refreshToken(localStorage.getItem("refresh_token"))
     } else if (response.status === 500) {
-        console.log("Виноват бэкэндер") // TODO обработать ошибку 422 как ошибку фронтендера
+        console.log("Виноват бэкэндер") 
+    } else if (response.status === 422) {
+            console.log("Виноват фронтендер")
     } else {
         const data = await response.json();
         console.log(data);
@@ -72,11 +72,12 @@ async function refreshToken(refresh_token) {
 
     if (response.status === 500) {
         console.log("Виноват бэкэндер")
-    } else if (response.status === 422 || response.status === 401) { //опять же 401 это протухший токен а 422 критическая ошибка фронтенда
+    } else if (response.status === 401) { 
         create_sign_in_container()
+    } else if (response.status === 422) {
+            console.log("Виноват фронтендер")
     } else {
         const data = await response.json();
-    // Сохраняем access token только в памяти приложения
         localStorage.setItem("accessToken", data.access_token);
         localStorage.setItem("refresh_token", data.refresh_token);
 
